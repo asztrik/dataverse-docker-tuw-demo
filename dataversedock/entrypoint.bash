d7d04ae44bba111a7de2a18d17c8e0449615325f
Tomasz Miksa <tmiksa@dataverse.ifs.tuwien.ac.at>
2019-08-09 23:46:50 +0200

    Test DOI + Custom meta v1

--- dataversedock/entrypoint.bash
+++ dataversedock/entrypoint.bash
@@ -4,8 +4,19 @@ if [ ! -e "/opt/glassfish4/glassfish/domains/domain1/applications/dataverse/WEB-
 	cd /opt/dv
 	./setupIT.bash
 fi
+
+sed -e 's/<jvm-options>-Ddoi.baseurlstring=https:\/\/mds.test.datacite.org<\/jvm-options>/<jvm-options>-Ddoi.baseurlstring=https:\/\/mds.test.datacite.org<\/jvm-options>\n<jvm-options>-Ddoi.username=AND.TEST<\/jvm-options>\n<jvm-options>-Ddoi.password=tuwiendoi<\/jvm-options>/' /opt/glassfish4/glassfish/domains/domain1/config/domain.xml > tmp
+rm /opt/glassfish4/glassfish/domains/domain1/config/domain.xml
+mv tmp /opt/glassfish4/glassfish/domains/domain1/config/domain.xml
+curl -X PUT -d 10.33581 http://localhost:8080/api/admin/settings/:Authority
+
+curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file /tmp/custom_metadata_block.tsv
+
+echo "Don't forget to enable the custom metadata block in the frontend!"
+
 cd /opt/glassfish4
 bin/asadmin stop-domain
 bin/asadmin start-domain
+
 sleep infinity
 
